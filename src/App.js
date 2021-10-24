2a417723a627bb7054d4d06f4c482e894a944720
tomafik <tomafik@gmail.com>
2021-10-24 20:26:17 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { Admin, Resource } from "react-admin";
+import polyglotI18nProvider from "ra-i18n-polyglot";
+
+import "./App.css";
+
+import authProvider from "./authProvider";
+import themeReducer from "./themeReducer";
+import { Login, Layout } from "./layout";
+import { Dashboard } from "./dashboard";
+import customRoutes from "./routes";
+import englishMessages from "./i18n/en";
+
+import visitors from "./visitors";
+import orders from "./orders";
+import products from "./products";
+import invoices from "./invoices";
+import categories from "./categories";
+import reviews from "./reviews";
+
+import dataProviderFactory from "./dataProvider";
+import fakeServerFactory from "./fakeServer";
+
+const i18nProvider = polyglotI18nProvider(locale => {
+  if (locale === "fr") {
+    return import("./i18n/fr").then(messages => messages.default);
+  }
+
+  // Always fallback on english
+  return englishMessages;
+}, "en");
+
+class App extends Component {
+  state = { dataProvider: null };
+
+  async componentDidMount() {
+    this.restoreFetch = await fakeServerFactory(
+      process.env.REACT_APP_DATA_PROVIDER
+    );
+
+    const dataProvider = await dataProviderFactory(
+      process.env.REACT_APP_DATA_PROVIDER
+    );
+
+    this.setState({ dataProvider });
+  }
+
+  componentWillUnmount() {
+    this.restoreFetch();
+  }
+
+  render() {
+    const { dataProvider } = this.state;
+
+    if (!dataProvider) {
+      return (
+        <div className="loader-container">
+          <div className="loader">Loading...</div>
+        </div>
+      );
+    }
+
+    return (
+      <Admin
+        title=""
+        dataProvider={dataProvider}
+        customReducers={{ theme: themeReducer }}
+        customRoutes={customRoutes}
+        authProvider={authProvider}
+        dashboard={Dashboard}
+        loginPage={Login}
+        layout={Layout}
+        i18nProvider={i18nProvider}
+      >
+        <Resource name="customers" {...visitors} />
+        <Resource name="commands" {...orders} options={{ label: "Orders" }} />
+        <Resource name="invoices" {...invoices} />
+        <Resource name="products" {...products} />
+        <Resource name="categories" {...categories} />
+        <Resource name="reviews" {...reviews} />
+      </Admin>
+    );
+  }
+}
+
+export default App;
